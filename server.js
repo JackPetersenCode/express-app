2d365de608ecb9c492eee405eef0993e5ea94d64
jackp <jackpetersenmusic@gmail.com>
2023-02-08 13:38:57 -0800

    again

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('just gonna send it');
 });
 app.get('/flower', (req, res) => {
+  console.log('flower')
   res.json({
     name: 'Dandelion',
     colour: 'Blue-ish'
